e8dcd6c58485a1e80cd4605a14ecec3224c3be73
wangwei <wangwei@ms-battery.cn>
2023-04-12 17:49:16 +0800

    优化

--- src/components/refTemplate.vue
+++ src/components/refTemplate.vue
@@ -1,6 +1,8 @@
 <template>
   <h1>ref-template</h1>
-  <p ref="elemRef">我是一行文字</p>
+  <p ref="elemRef">我是一行文字elemRef</p>
+  <p :ref="elemRef1">我是一行文字elemRef1</p>
+  <p :ref="elemRef2">我是一行文字elemRef2</p>
 </template>
 
 <script>
@@ -10,13 +12,23 @@ export default {
   name: "RefTemplate",
   setup() {
     const elemRef = ref();
+    // 获取多个DOM结构
+    const elemRef1 = (el)=>{
+      console.log(el)
+    };
+    const elemRef2 = (el)=>{
+      console.log(el)
+    };
 
     onMounted(() => {
       console.log("ref template", elemRef.value.innerHTML, elemRef.value);
+      console.log(elemRef1.value)
     });
 
     return {
       elemRef,
+      elemRef1,
+      elemRef2
     };
   },
 };
